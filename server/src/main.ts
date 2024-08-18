b1f5e6cadec3df327b5391347b86aa0ec13007c5
maryamaljanabi <maryam.aljanabi135@gmail.com>
2024-08-18 12:19:46 +0300

    initialize server

---
+++ server/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const PORT = process.env.API_PORT || 5000;
+  await app.listen(PORT);
+}
+bootstrap();
